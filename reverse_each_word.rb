7928bee9369ba80a88ee9edb6580a98baf82eb6d
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-05-13 01:49:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,4 +8,11 @@ def reverse_each_word(sentence)
 end
 
 
-def 
+def reverse_each_word(sentence)
+  new_sentence = []
+  array = sentence.split(" ")
+  array.collect do |word|
+  new_sentence << word.reverse
+  end
+  new_sentence.join(" ")
+end
